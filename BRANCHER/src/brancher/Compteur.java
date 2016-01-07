105001a126befb579fb36d2988a7a32a4b74a175
bettamedislem <bettamedislem@gmail.com>
2016-01-07 14:54:46 +0100

    added ketima changes

---
+++ BRANCHER/src/brancher/Compteur.java
@@ -0,0 +1,49 @@
+package brancher;
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+
+
+public class Compteur extends JFrame {
+
+	private JPanel contentPane;
+	public int Numero , NIndice,Adindice;
+	public int[] type={5,3,2};
+	public int Calculons()
+	{
+		int i =1;
+		return type[i]*(Adindice-NIndice);
+		
+	}
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Compteur frame = new Compteur();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public Compteur() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		contentPane.setLayout(new BorderLayout(0, 0));
+		setContentPane(contentPane);
+	}
+
+}
