a9cec9ae1a51e3305608a2f2edcbba991371446b
bettamedislem <bettamedislem@gmail.com>
2016-01-07 14:36:03 +0100

    Added Change updated

---
+++ BRANCHER/src/brancher/Abonne.java
@@ -0,0 +1,50 @@
+package brancher;
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+
+
+public class Abonne extends JFrame {
+
+	private JPanel contentPane;
+	public int Numero =8878744;
+	public String Nom, Adress;
+	public int getNum()
+	{
+		
+		return this.Numero;
+	}
+	public int[] categorie={1,2,3,4};
+	
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Abonne frame = new Abonne();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public Abonne() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		contentPane.setLayout(new BorderLayout(0, 0));
+		setContentPane(contentPane);
+	}
+
+}
---
+++ FACTURER/src/facturer/Facture.java
@@ -0,0 +1,14 @@
+package facturer;
+
+
+  
+public class Facture {
+
+	
+	Consommations[] consom=new Consommations[3];
+	
+	
+	 public static void main(String[] args){
+			System.out.println("test");
+			}
+}
